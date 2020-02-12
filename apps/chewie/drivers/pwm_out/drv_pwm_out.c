813e280190732df068a82ce01877d6a170b5ef11
Hotice0 <hotice0@qq.com>
2020-02-13 01:58:30 +0800

    [ADD]add drv_pwm_out: lower api for pwm device

--- apps/chewie/drivers/pwm_out/drv_pwm_out.c
+++ apps/chewie/drivers/pwm_out/drv_pwm_out.c
@@ -3,103 +3,174 @@
  ****************************************************************************/
 #include "drv_pwm_out.h"
 
-int init_pwm_device()
+int drv_pwm_init(const char *devpath)
 {
-    int i = 0;
-    int j = 0;
+    int fd = 0;
+    if (!devpath) {
+        printf("drv_pwm_init(devpath: NULL) failed\n");
+        goto errout;
+    }
+    fd = open(devpath, O_RDONLY);
+    if (fd < 0) {
+        printf("drv_pwm_init: open(devpath: %s, ...) pwm dev failed with errno : %d\n", devpath, errno);
+        goto errout;
+    }
+    return fd;
+errout:
+    fflush(stdout);
+    return -1;
+}
+
+int drv_pwm_start(int fd)
+{
     int ret = 0;
-    char devpath[50] = {0};
-
-    if (!g_pwmstate.initialized) {
-        // init g_pwmstate
-        g_pwmstate.freq = CONFIG_CHEWIE_PWM_FREQUENCY;
-        for (i = 0; i < CHEWIE_NUM_OF_PWM_DEV; ++i) {
-            g_pwmstate.pwms_info[i].frequency = CONFIG_CHEWIE_PWM_FREQUENCY;
-            g_pwmstate.pwm_devs_fd[i] = -1;
-            for (j = 0; j < 4 && i * 4 + j < CONFIG_CHEWIE_NUM_OF_CHANNELS; ++j) {
-                g_pwmstate.pwms_info[i].channels[j].channel = j + 1;
-                g_pwmstate.pwms_info[i].channels[j].duty = CONFIG_CHEWIE_PWM_DUTY_INIT_VALUE;
-            }
-        }
-
-        // init all pwm device
-        for (i = 0; i < CHEWIE_NUM_OF_PWM_DEV; ++i) {
-            sprintf(devpath, "%s%d", CONFIG_CHEWIE_PWM_DEVICE_PATH_PREFIX, i);
-            printf("dev %s init\n", devpath);
-
-            // open the pwm device
-            g_pwmstate.pwm_devs_fd[i] = open(devpath, O_RDONLY);
-            if (g_pwmstate.pwm_devs_fd[i] < 0) {
-                printf("init_pwm_device: open %s failed: %d\n", devpath, errno);
-                goto errout;
-            }
-
-            // send control info to pwm device
-            ret = ioctl(g_pwmstate.pwm_devs_fd[i], PWMIOC_SETCHARACTERISTICS, (unsigned long)((uintptr_t)&g_pwmstate.pwms_info[i]));
-            if (ret < 0) {
-                printf("init_pwm_device: ioctl(PWMIOC_SETCHARACTERISTICS) failed: %d\n", errno);
-                goto errout_with_dev;
-            }
-
-            // start the pwm device
-            ret = ioctl(g_pwmstate.pwm_devs_fd[i], PWMIOC_START, 0);
-            if (ret < 0) {
-                printf("init_pwm_device: ioctl(PWMIOC_START) failed: %d\n", errno);
-                goto errout_with_dev;
-            }
-
-            printf("success init and start pwm device : %s\n", devpath);
-        }
-        g_pwmstate.initialized = true;
+    // start the pwm device
+    ret = ioctl(fd, PWMIOC_START, 0);
+    if (ret < 0) {
+        printf("drv_pwm_start: ioctl(PWMIOC_START) failed: %d\n", errno);
+        goto errout;
     }
-    else
+    return 0;
+errout:
+    fflush(stdout);
+    return -1;
+}
+
+int drv_pwm_stop(int fd)
+{
+    int ret = 0;
+    ret = ioctl(fd, PWMIOC_STOP, 0);
+    if (ret < 0)
     {
-        printf("pwm device is already inited\n");
-    }
-    return OK;
-errout_with_dev:
-    for (i = 0; i < CHEWIE_NUM_OF_PWM_DEV; ++i)
-    {
-        if (g_pwmstate.pwm_devs_fd[i] >= 0)
-            close(g_pwmstate.pwm_devs_fd[i]);
+        printf("drv_pwm_stop: ioctl(PWMIOC_STOP) failed: %d\n", errno);
+        goto errout;
     }
 errout:
     fflush(stdout);
-    return ERROR;
+    return -1;
 }
 
-int set_pwm_duty(uint8_t channel, ub16_t duty)
+int drv_pwm_update_info(int fd, const struct pwm_info_s *p_init_info_s)
 {
     int ret = 0;
-    int i = 0;
-    uint8_t index_dev = channel % 4 == 0 ? channel / 4 - 1 : channel / 4;
-
-    g_pwmstate.pwms_info[index_dev].channels[channel].duty = duty;
-
     // send control info to pwm device
-    ret = ioctl(g_pwmstate.pwm_devs_fd[index_dev], PWMIOC_SETCHARACTERISTICS, (unsigned long)((uintptr_t)&g_pwmstate.pwms_info[index_dev]));
+    ret = ioctl(fd, PWMIOC_SETCHARACTERISTICS, (unsigned long)((uintptr_t)p_init_info_s));
     if (ret < 0) {
-        printf("init_pwm_device: ioctl(PWMIOC_SETCHARACTERISTICS) failed: %d\n", errno);
-        goto errout_with_dev;
+        printf("drv_pwm_init: ioctl(fd: %d) failed with code %d\n", fd, errno);
+        goto errout;
     }
-    return OK;
-
-errout_with_dev:
-    for (i = 0; i < CHEWIE_NUM_OF_PWM_DEV; ++i) {
-        if (g_pwmstate.pwm_devs_fd[i] >= 0)
-            close(g_pwmstate.pwm_devs_fd[i]);
-    }
+    return 0;
+errout:
     fflush(stdout);
-    return ERROR;
+    return -1;
 }
 
-int uninit_pwm_device()
+void drv_pwm_uninit(int fd)
 {
-    int i = 0;
-    g_pwmstate.initialized = false;
-    for (i = 0; i < CHEWIE_NUM_OF_PWM_DEV; ++i) {
-        if (g_pwmstate.pwm_devs_fd[i] >= 0)
-            close(g_pwmstate.pwm_devs_fd[i]);
-    }
-    return OK;
+    close(fd);
 }
+
+
+
+// int init_pwm_device()
+// {
+//     int i = 0;
+//     int j = 0;
+//     int ret = 0;
+//     char devpath[50] = {0};
+
+//     if (!g_pwmstate.initialized) {
+//         // init g_pwmstate
+//         g_pwmstate.freq = CONFIG_CHEWIE_PWM_FREQUENCY;
+//         for (i = 0; i < CHEWIE_NUM_OF_PWM_DEV; ++i) {
+//             g_pwmstate.pwms_info[i].frequency = CONFIG_CHEWIE_PWM_FREQUENCY;
+//             g_pwmstate.pwm_devs_fd[i] = -1;
+//             for (j = 0; j < 4 && i * 4 + j < CONFIG_CHEWIE_NUM_OF_CHANNELS; ++j) {
+//                 g_pwmstate.pwms_info[i].channels[j].channel = j + 1;
+//                 g_pwmstate.pwms_info[i].channels[j].duty = CONFIG_CHEWIE_PWM_DUTY_INIT_VALUE;
+//             }
+//         }
+
+//         // init all pwm device
+//         for (i = 0; i < CHEWIE_NUM_OF_PWM_DEV; ++i) {
+//             sprintf(devpath, "%s%d", CONFIG_CHEWIE_PWM_OUT_DEVICE_PATH_PREFIX, i);
+//             printf("dev %s init\n", devpath);
+
+//             // open the pwm device
+//             g_pwmstate.pwm_devs_fd[i] = open(devpath, O_RDONLY);
+//             if (g_pwmstate.pwm_devs_fd[i] < 0) {
+//                 printf("init_pwm_device: open %s failed: %d\n", devpath, errno);
+//                 goto errout;
+//             }
+
+//             // send control info to pwm device
+//             ret = ioctl(g_pwmstate.pwm_devs_fd[i], PWMIOC_SETCHARACTERISTICS, (unsigned long)((uintptr_t)&g_pwmstate.pwms_info[i]));
+//             if (ret < 0) {
+//                 printf("init_pwm_device: ioctl(PWMIOC_SETCHARACTERISTICS) failed: %d\n", errno);
+//                 goto errout_with_dev;
+//             }
+
+//             // start the pwm device
+//             ret = ioctl(g_pwmstate.pwm_devs_fd[i], PWMIOC_START, 0);
+//             if (ret < 0) {
+//                 printf("init_pwm_device: ioctl(PWMIOC_START) failed: %d\n", errno);
+//                 goto errout_with_dev;
+//             }
+
+//             printf("success init and start pwm device : %s\n", devpath);
+//         }
+//         g_pwmstate.initialized = true;
+//     }
+//     else
+//     {
+//         printf("pwm device is already inited\n");
+//     }
+//     return OK;
+// errout_with_dev:
+//     for (i = 0; i < CHEWIE_NUM_OF_PWM_DEV; ++i)
+//     {
+//         if (g_pwmstate.pwm_devs_fd[i] >= 0)
+//             close(g_pwmstate.pwm_devs_fd[i]);
+//     }
+// errout:
+//     fflush(stdout);
+//     return ERROR;
+// }
+
+// int set_pwm_duty(uint8_t channel, ub16_t duty)
+// {
+//     int ret = 0;
+//     int i = 0;
+//     uint8_t index_dev = channel % 3 == 0 ? channel / 3 - 1 : channel / 3;
+
+//     g_pwmstate.pwms_info[index_dev].channels[channel].duty = duty;
+//     #ifdef PWM_DEBUG
+//     printf("set_pwm_duty(channel: %d, duty: %d) index_dev: %d", (int)channel, (int)duty, (int)index_dev);
+//     #endif
+//     // send control info to pwm device
+//     ret = ioctl(g_pwmstate.pwm_devs_fd[index_dev], PWMIOC_SETCHARACTERISTICS, (unsigned long)((uintptr_t)&g_pwmstate.pwms_info[index_dev]));
+//     if (ret < 0) {
+//         printf("init_pwm_device: ioctl(PWMIOC_SETCHARACTERISTICS) failed: %d\n", errno);
+//         goto errout_with_dev;
+//     }
+//     return OK;
+
+// errout_with_dev:
+//     for (i = 0; i < CHEWIE_NUM_OF_PWM_DEV; ++i) {
+//         if (g_pwmstate.pwm_devs_fd[i] >= 0)
+//             close(g_pwmstate.pwm_devs_fd[i]);
+//     }
+//     fflush(stdout);
+//     return ERROR;
+// }
+
+// int uninit_pwm_device()
+// {
+//     int i = 0;
+//     g_pwmstate.initialized = false;
+//     for (i = 0; i < CHEWIE_NUM_OF_PWM_DEV; ++i) {
+//         if (g_pwmstate.pwm_devs_fd[i] >= 0)
+//             close(g_pwmstate.pwm_devs_fd[i]);
+//     }
+//     return OK;
+// }
--- apps/chewie/drivers/pwm_out/drv_pwm_out.h
+++ apps/chewie/drivers/pwm_out/drv_pwm_out.h
@@ -13,61 +13,28 @@
 
 #include <nuttx/drivers/pwm.h>
 
-
 /****************************************************************************
  * Pre-processor Definitions
  ****************************************************************************/
-
-/**
- * The path of the pwm out device is named like,
- * CHEWIE_PWM_OUT_DEFAULT_DEVICE_PATH_PREFIX + "0" | + "1" ...
- * /dev/pwm_out0
- * /dev/pwm_out1
- */
-#define CHEWIE_PWM_OUT_DEFAULT_DEVICE_PATH_PREFIX "/dev/pwm_out"
-/**
- * The value of pwm device frequency(Hz).
- */
-#define CHEWIE_PWM_FREQUENCY 400
 /**
  * ESC (Electronic Speed Control) relate.
  */
-#define CHEWIE_PWM_ESC_ARMED_PULSE_US 900
-#define CHEWIE_PWM_ESC_MIN_PULSE_US 1000
-#define CHEWIE_PWM_ESC_MAX_PULSE_US 2000
-
-#define CHEWIE_PWM_ARMED_DUTY
-#define CHEWIE_NUM_OF_PWM_DEV ((CONFIG_CHEWIE_NUM_OF_CHANNELS % 4) == 0 ? CONFIG_CHEWIE_NUM_OF_CHANNELS / 4 : CONFIG_CHEWIE_NUM_OF_CHANNELS / 4 + 1)
-
-/****************************************************************************
- * Private Types
- ****************************************************************************/
-struct pwm_state_s
-{
-  bool initialized;
-  struct pwm_info_s pwms_info[CHEWIE_NUM_OF_PWM_DEV];
-  int pwm_devs_fd[CHEWIE_NUM_OF_PWM_DEV];
-  uint32_t freq;
-};
-
-/****************************************************************************
- * Private Data
- ****************************************************************************/
-struct pwm_state_s g_pwmstate;
+//#define CHEWIE_PWM_ESC_ARMED_PULSE_US 900
+//#define CHEWIE_PWM_ESC_MIN_PULSE_US 1000
+//#define CHEWIE_PWM_ESC_MAX_PULSE_US 2000
 
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
-
 /**
  * init pwm device
  */
-int init_pwm_device();
+int drv_pwm_init(const char *devpath);
 
-/**
- * set current duty of pwm channel
- */
-int set_pwm_duty(uint8_t channel, ub16_t duty);
+int drv_pwm_start(int fd);
 
+int drv_pwm_stop(int fd);
 
-int uninit_pwm_device();
+int drv_pwm_update_info(int fd, const struct pwm_info_s *p_init_info_s);
+
+void drv_pwm_uninit(int fd);
